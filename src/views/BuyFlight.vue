5c629ec81a84b643d7b1b646e3805174b7d6ccc1
Andrew Bowden <andrew@ebowden.com>
2020-03-19 14:59:41 -0600

    Finished

--- src/views/BuyFlight.vue
+++ src/views/BuyFlight.vue
@@ -1,32 +1,36 @@
 <template>
 <div v-if="this.$root.$data.currentFlightBeingPurchased.length > 0">
-<div class="flightDetails">
-    <h2><u>Flight details</u></h2>
-    <p><strong>Flight from: </strong>{{this.$root.$data.startingCity}}</p>
-    <p><strong>Flight to: </strong>{{this.$root.$data.currentFlightBeingPurchased[0].city}}</p>
-    <p> Departs at: {{computeTimeFromNow(this.$root.$data.currentFlightBeingPurchased[0].time_to_takeoff)}}</p>
-    <p><strong>Seat Type:</strong></p>
-    <div class="dropdown">
-      <button class="dropbtn">{{seatType}}</button>
-      <div class="dropdown-content">
-        <a @click=" seatType= 'Economy'">Economy</a>
-        <a @click=" seatType= 'First Class'">First Class</a>
-       </div>
-    </div>
-      <!-- <p v-if="seatType === 'Economy'"> <strong>Ticket Price: </strong>${{this.$root.$data.currentFlightBeingPurchased[0].base_price.toFixed(2)}}</p>
-      <p v-else> <strong>Ticket Price: </strong>${{this.$root.$data.currentFlightBeingPurchased[0].first_class_price.toFixed(2)}}</p> -->
+  <div class="flightDetails">
+      <h2><u>Flight details</u></h2>
+      <p><strong>Flight From: </strong>{{this.$root.$data.startingCity}}</p>
+      <p><strong>Flight To: </strong>{{this.$root.$data.currentFlightBeingPurchased[0].city}}</p>
+      <p> <strong>Departs at: </strong>{{computeTimeFromNow(this.$root.$data.currentFlightBeingPurchased[0].time_to_takeoff)}}</p>
       <p><strong>Ticket Price: </strong>${{price()}}</p>
-		<br/>
-</div>
+      <div class="seat-type">
+        <p class="seat-type-text"><strong>Seat Type: </strong></p>
+        <div class="dropdown">
+          <button class="dropbtn">{{seatType}}</button>
+          <div class="dropdown-content">
+            <a @click=" seatType= 'Economy'">Economy</a>
+            <a @click=" seatType= 'First Class'">First Class</a>
+          </div>
+        </div>
+      </div>
+      <br/>
+  </div>
+  <h2><u>Personal Information</u></h2>
   <h3>Before purchasing your flight to {{this.$root.$data.currentFlightBeingPurchased[0].city}} we need just a bit more information about you.</h3>
   <form>
-    <input type="text" placeholder="First Name" v-model="firstName">
-    <input type="text" placeholder="Last Name">
-    <router-link to="/myflights">
-    <button @click="addFlight" >Purchase Flight</button>
-    </router-link>
-
+    <div class="additional-information">
+      <input type="text" placeholder="First Name" v-model="firstName">
+      <input type="text" placeholder="Last Name">
+      <input type="text" placeholder="Email Address">
+      <input type="text" placeholder="Phone Number">
+    </div>
   </form>
+   <router-link to="/myflights">
+        <button class="purchase-button" @click="addFlight">Purchase Flight</button>
+      </router-link>
 </div>
 </template>
 
@@ -72,21 +76,57 @@ export default {
 
 
 <style scoped>
+.seat-type {
+display: flex;
+}
+
+input {
+  width: 20%;
+  margin: 10px 0px 10px 0px;
+}
+.seat-type-text {
+  margin-right: 10px;
+  margin-top: 5px;
+  margin-bottom: 0;
+}
+
+.additional-information {
+  display: flex;
+  flex-direction: column;
+}
+
+.purchase-button {
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+  background-image: -webkit-gradient(linear, left bottom, left top, color-stop(0.16, rgb(207, 207, 207)), color-stop(0.79, rgb(252, 252, 252)));
+  background-image: -moz-linear-gradient(center bottom, rgb(207, 207, 207) 16%, rgb(252, 252, 252) 79%);
+  background-image: linear-gradient(to top, rgb(207, 207, 207) 16%, rgb(252, 252, 252) 79%); 
+  padding: 3px;
+  border: 1px solid #000;
+  color: black;
+  text-decoration: none;
+  width: 120px;
+  height: 30px;
+  font-size: 12px;
+  margin-left: 0px;
+}
+
 .dropbtn {
   background-color: #d3d3d3;
   color: black;
-  padding: 16px;
-  font-size: 16px;
+  margin: 0;
+  font-size: 12px;
   border: none;
   cursor: pointer;
-  margin: 0;
+  height: 30px;
 }
 
 /* The container <div> - needed to position the dropdown content */
 .dropdown {
   position: relative;
   display: inline-block;
-  width: 160px;
+  width: 120px;
 }
 
 /* Dropdown Content (Hidden by Default) */
